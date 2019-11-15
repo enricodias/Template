2f8f14adfbdfc6a7fd18f645a0abb8c59821914a
enricodias <enricodias@gmail.com>
2019-11-15 15:36:09 -0300

    rename variables to camelCase style

--- tests/TemplateTest.php
+++ tests/TemplateTest.php
@@ -7,75 +7,75 @@ final class TemplateTest extends TestCase
 {
     public function testLoad()
     {
-        $Template = new Template('tests/samples/a.tpl');
+        $template = new Template('tests/samples/a.tpl');
 
-        $this->assertSame('Template A. {$var}', $Template->getContent());
+        $this->assertSame('Template A. {$var}', $template->getContent());
 
-        $Template->load('tests/samples/b.tpl');
+        $template->load('tests/samples/b.tpl');
 
-        $this->assertSame('Template B.', $Template->getContent());
+        $this->assertSame('Template B.', $template->getContent());
     }
 
     public function testWithoutTemplateFile()
     {
-        $Template = new Template('tests/samples/wrong_path.tpl');
+        $template = new Template('tests/samples/wrong_path.tpl');
 
-        $this->assertSame('', $Template->getContent());
+        $this->assertSame('', $template->getContent());
     }
 
     public function testReplace()
     {
-        $Template = new Template('tests/samples/a.tpl');
+        $template = new Template('tests/samples/a.tpl');
 
-        $Template->replace('var', '{$var2}');
-        $this->assertSame('Template A. {$var2}', $Template->getContent());
+        $template->replace('var', '{$var2}');
+        $this->assertSame('Template A. {$var2}', $template->getContent());
 
-        $Template->replace('var2', 'value');
-        $this->assertSame('Template A. value', $Template->getContent());
+        $template->replace('var2', 'value');
+        $this->assertSame('Template A. value', $template->getContent());
     }
 
     public function testReload()
     {
-        $Template = new Template('tests/samples/a.tpl');
+        $template = new Template('tests/samples/a.tpl');
 
-        $Template->reload('tests/samples/b.tpl');
-        $this->assertSame('Template A. {$var}'.'Template B.', $Template->getContent());
+        $template->reload('tests/samples/b.tpl');
+        $this->assertSame('Template A. {$var}'.'Template B.', $template->getContent());
 
-        $Template->reload('tests/samples/b.tpl');
-        $this->assertSame('Template A. {$var}'.'Template B.'.'Template B.', $Template->getContent());
+        $template->reload('tests/samples/b.tpl');
+        $this->assertSame('Template A. {$var}'.'Template B.'.'Template B.', $template->getContent());
     }
 
     public function testBlocks()
     {
-        $Template = new Template('tests/samples/blocks_test.tpl');
+        $template = new Template('tests/samples/blocks_test.tpl');
 
-        $Template->getBlock('header');
-        $Template->makeBlock();
+        $template->getBlock('header');
+        $template->makeBlock();
 
-        $Template->getBlock('row');
+        $template->getBlock('row');
 
-        $Template->reloadBlock();
-        $Template->reloadBlock();
+        $template->reloadBlock();
+        $template->reloadBlock();
 
-        $Template->getBlock('footer');
-        $Template->reloadBlock();
+        $template->getBlock('footer');
+        $template->reloadBlock();
 
-        $this->assertSame(file_get_contents('tests/samples/blocks_result.tpl'), $Template->getContent());
+        $this->assertSame(file_get_contents('tests/samples/blocks_result.tpl'), $template->getContent());
     }
 
     public function testToString()
     {
-        $Template = new Template('tests/samples/b.tpl');
+        $template = new Template('tests/samples/b.tpl');
 
-        $this->assertSame('Template B.', $Template->__toString());
+        $this->assertSame('Template B.', $template->__toString());
     }
 
     public function testPublish()
     {
         $this->expectOutputString('Template B.');
 
-        $Template = new Template('tests/samples/b.tpl');
-        $Template->publish();
+        $template = new Template('tests/samples/b.tpl');
+        $template->publish();
     }
 
 }
